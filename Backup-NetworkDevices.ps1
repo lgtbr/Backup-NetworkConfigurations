05be41a1de885f6d0f70a54fdbbf789bd43f5284
Louis GATTABRUSI <103209976+lgtbr@users.noreply.github.com>
2023-03-06 09:39:36 +0100

    Update Backup-NetworkDevices.ps1

--- Backup-NetworkDevices.ps1
+++ Backup-NetworkDevices.ps1
@@ -1,43 +1,70 @@
 #$secureString = Read-Host -AsSecureString | ConvertFrom-SecureString
 
-###############################################################################################
-#                                                                                             #
-# DESCRIPTION                                                                                 #
-#                                                                                             #
-# Ce script permet la sauvegarde de la configration d'un FortiSwitch (ou d'un autre           #
-# équipement permettant la connexion en SSH puis l'export via TFTP).                          #
-#                                                                                             #
-# Il fonctionne uniquement pour un équipement. Pour l'utiliser sur plusieurs équipements, il  #
-# faut faire une copie de ce fichier puis le reconfigurer pour l'équipement en question.      #
-#                                                                                             #
-###############################################################################################
+<#
+    .SYNOPSIS
+    Adds a file name extension to a supplied name.
+
+    .DESCRIPTION
+    Adds a file name extension to a supplied name.
+    Takes any strings for the file name or extension.
+
+    .PARAMETER Name
+    Specifies the file name.
+
+    .PARAMETER Extension
+    Specifies the extension. "Txt" is the default.
+
+    .INPUTS
+    None. You cannot pipe objects to Add-Extension.
+
+    .OUTPUTS
+    System.String. Add-Extension returns a string with the extension or file name.
+
+    .EXAMPLE
+    PS> extension -name "File"
+    File.txt
+
+    .EXAMPLE
+    PS> extension -name "File" -extension "doc"
+    File.doc
+
+    .EXAMPLE
+    PS> extension "File" "doc"
+    File.doc
+
+    .LINK
+    Online version: http://www.fabrikam.com/extension.html
+
+    .LINK
+    Set-Item
+#>
 
 
+############################################################################################
+#                                                                                          #
+#                               0 - Initialisation & checks                                #
+#                                                                                          #
+############################################################################################
 
-###############################################################################################
-#                                                                                             #
-# 0 - PREREQUIS                                                                               #
-#                                                                                             #
-# / ! \ Télécharger TFTP Server de SolarWinds à cette adresse :                               #
-# https://www.solarwinds.com/fr/free-tools/free-tftp-server                                   #
-#                                                                                             #
-# METTRE LE SERVICE TFTP SERVER EN MODE DEMARRAGE MANUEL                                      #
-# Cela permettra de lancer le service uniquement quand on en a besoin via le script           #
-#                                                                                             #
-# Changer également le dossier racine du serveur TFTP afin de le faire correspondre à         #
-# l'emplacement de la variable $folderLocation ci-dessous                                     #
-#                                                                                             #
-# Merci de compléter l'ensemble des variables ci-dessous puis lancer le script une première   #
-# fois manuellement afin de renseigner le mot de passe de l'équipement                        #
-#                                                                                             #
-###############################################################################################
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptPath = Split-Path $scriptPath -Parent
 
 # Emplacement du fichier JSON
-if (Get-Content "C:\Scripting\V2\config\network_equipment.json" -erroraction 'silentlycontinue' )  {
-    $networkEquipmentJSON = Get-Content "C:\Scripting\V2\config\network_equipment.json" | ConvertFrom-Json
-    Write-Host "OK - JSON file has been found and loaded" -ForegroundColor green
+if (Get-Content "$scriptPath\config\networkDevices.json" -erroraction 'silentlycontinue' )  {
+    $networkEquipmentJSON = Get-Content "$scriptPath\config\networkDevices.json" | ConvertFrom-Json
+    Write-Host "✔️ - JSON file has been found and loaded" -ForegroundColor green
 } else {
-    Write-Host "NOK - JSON file has not been found and loaded, please check path and / or content" -ForegroundColor red
+    if(Test-Path -Path "$scriptPath\config") {
+        Write-Host "✔️ - The folder $scriptPath\config already exist." -ForegroundColor green
+    } else {
+        Write-Host "❌ - The folder $scriptPath\config does not exist !" -ForegroundColor red
+        Write-Host "⚠ - Creating folder $scriptPath\config" -ForegroundColor yellow
+        New-Item "$scriptPath\config" -ItemType Directory | Out-Null
+    }
+    Write-Host "❌ - JSON file has not been found, file will be downloaded..." -ForegroundColor red
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lgtbr/Backup-NetworkDevices/main/config/networkDevices.json" -OutFile "$scriptPath\config\networkDevices.json"
+    Write-Host "⚠ - File has been downloaded, please configure the file before executing the script" -ForegroundColor yellow
+    Write-Host "⚠ - You can find more infornations on GitHub page https://github.com/lgtbr/Backup-NetworkDevices" -ForegroundColor yellow
     break
 }
 
@@ -46,58 +73,11 @@ $folderLocation = $networkEquipmentJSON.configBackupsLocation | % {$_.replace("/
 
 $actualDate = Get-Date -Format "MM.dd.yyyy_HH-mm-ss"
 
-
-###############################################################################################
-#                                                                                             #
-# 1 - DEMARRAGE DU SERVICE TFTP                                                               #
-#                                                                                             #
-###############################################################################################
-
-$serviceState = Get-Service -Name "TFTPServer"
-
-if ($serviceState.Status -eq "Stopped") {
-    Start-Service -Name "TFTPServer"
-    Write-Host "OK - Starting Open TFTP Server service..." -ForegroundColor yellow
-} else {
-    Write-Host "OK - Open TFTP Server service is already started !" -ForegroundColor green
-}
-
-###############################################################################################
-#                                                                                             #
-# 1 - PREPARATION ET INSTALLATION POSH-SSH                                                    #
-#                                                                                             #
-# On va voir si le dossier $folderName existe, si ce n'est pas le cas, on le crée             #
-# Idem pour le module Posh-SSH, si il n'est pas installé, on l'installe                       #
-#                                                                                             #
-###############################################################################################
-
-if(Test-Path -Path $folderLocation) {
-     Write-Host "OK - The folder $folderLocation already exist." -ForegroundColor green
-} else {
-    Write-Host "NOK - The folder $folderLocation does not exist !" -ForegroundColor red
-    Write-Host "OK - Creation of the folder $folderLocation..." -ForegroundColor yellow
-    New-Item $folderLocation -ItemType Directory
-}
-
-if (Get-Module -ListAvailable -Name "Posh-SSH") {
-    Import-Module Posh-SSH
-    Write-Host "OK - The module Posh-SSH has been detected succesfully." -ForegroundColor green
-} else {
-    Write-Host "OK - The module Posh-SSH is not installed !" -ForegroundColor red
-    Write-Host "OK - Please wait until the installation of Posh-SSH is being processed..." -ForegroundColor yellow
-    Install-Module Posh-SSH -Confirm:$false -Force
-}
-
-
-###############################################################################################
-#                                                                                             #
-# 3 - CONNEXION SSH                                                                           #
-#                                                                                             #
-# Connexion à l'équipement en SSH, export de la config via TFTP et on ferme la connexion      #
-# Rangement du fichier de config. dans un dossier qui porte le nom de l'équipement            #
-# Si ce dossier n'existe pas, on le crée                                                      #
-#                                                                                             #
-###############################################################################################
+############################################################################################
+#                                                                                          #
+#                                      1 - Functions                                       #
+#                                                                                          #
+############################################################################################
 
 function Get-NetworkEquipments($object) {
     $keys = $object | Get-Member -MemberType NoteProperty | Where-Object -Property Name -notlike "config*" | Select-Object -ExpandProperty Name
@@ -108,7 +88,7 @@ function Get-NetworkEquipments($object) {
 
 function Backup-NetworkEquipmentsConfig {
     foreach ($equipment in $networkEquipments) {
-        Write-Host "OK - Connecting to $($equipment.hostname) [$($equipment.informations.ip)]" -ForegroundColor Yellow
+        Write-Host "⚠️ - Connecting to $($equipment.hostname) [$($equipment.informations.ip)]" -ForegroundColor Yellow
 
         $equipmentCredentials = New-Object System.Management.Automation.PSCredential -ArgumentList $equipment.informations.username, (ConvertTo-SecureString $equipment.informations.password -Force)
         New-SSHSession -ComputerName $equipment.informations.ip -Credential $equipmentCredentials -AcceptKey | Out-Null
@@ -118,45 +98,89 @@ function Backup-NetworkEquipmentsConfig {
         Start-Sleep -s 1
         $SSHStream.read() | Out-Null
 
-        Write-Host "OK - Sending backup command to $($equipment.hostname) [$($equipment.informations.ip)]" -ForegroundColor Green
+        Write-Host "✔️ - Sending backup command to $($equipment.hostname) [$($equipment.informations.ip)]" -ForegroundColor Green
         $SSHStream.WriteLine("$($equipment.backupCommand | % {$_.replace('$fileName',"CFG_$($equipment.hostname)-$actualDate")} | % {$_.replace('$TFTPServer',"$($networkEquipmentJSON.configTFTPServerIP)")})")
         Start-Sleep -s 20
         $SSHStream.read() | Out-Null
 
-        Write-Host "OK - Disconnecting from $($equipment.hostname) [$($equipment.informations.ip)]" -ForegroundColor Green
+        Write-Host "⚠️ - Disconnecting from $($equipment.hostname) [$($equipment.informations.ip)]" -ForegroundColor Yellow
         Remove-SSHSession -Index 0 | Out-Null
 
         if(Test-Path -Path "$folderLocation\$($equipment.hostname)") {
-            Write-Host "OK - The folder $folderLocation\$($equipment.hostname) already exist." -ForegroundColor green
+            Write-Host "✔️ - The folder $folderLocation\$($equipment.hostname) already exist." -ForegroundColor green
         } else {
-            Write-Host "NOK - The folder $folderLocation\$($equipment.hostname) does not exist !" -ForegroundColor red
+            Write-Host "❌ - The folder $folderLocation\$($equipment.hostname) does not exist !" -ForegroundColor red
             New-Item "$folderLocation\$($equipment.hostname)" -ItemType Directory
-            Write-Host "OK - Folder $folderLocation\$($equipment.hostname) has been created" -ForegroundColor green
+            Write-Host "✔️ - Folder $folderLocation\$($equipment.hostname) has been created" -ForegroundColor green
         }
 
         if(Test-Path -Path "$folderLocation\CFG_$($equipment.hostname)-$actualDate.*") {
             Move-Item -Path "$folderLocation\CFG_$($equipment.hostname)-$actualDate.*" -Destination "$folderLocation\$($equipment.hostname)\" -Force
         } else {
-            Write-Host "NOK - The file $folderLocation\$CFG_$($equipment.hostname)-$actualDate.* has not been moved because the file is unavailable !" -ForegroundColor red
+            Write-Host "❌ - The file $folderLocation\$CFG_$($equipment.hostname)-$actualDate.* has not been moved because the file is unavailable !" -ForegroundColor red
         }
         Get-ChildItem -File -Path "$folderLocation\$($equipment.hostname)" -Filter *.* -Force | Sort -Property CreationTime -Descending | Select-Object -Skip $networkEquipmentJSON.configRestorePoints | Remove-Item
     }
 }
 
+############################################################################################
+#                                                                                          #
+#                                 2 - Starting TFTP Server                                 #
+#                                                                                          #
+############################################################################################
+
+$serviceState = Get-Service -Name "TFTPServer"
+
+if ($serviceState.Status -eq "Stopped") {
+    Start-Service -Name "TFTPServer"
+    Write-Host "⚠  - Starting Open TFTP Server service..." -ForegroundColor yellow
+} else {
+    Write-Host "✔️ - Open TFTP Server service is already started !" -ForegroundColor green
+}
+
+############################################################################################
+#                                                                                          #
+#                                3 - Check module Posh-SSH                                 #
+#                                                                                          #
+############################################################################################
+
+if(Test-Path -Path $folderLocation) {
+     Write-Host "✔️ - The folder $folderLocation already exist." -ForegroundColor green
+} else {
+    Write-Host "❌ - The folder $folderLocation does not exist !" -ForegroundColor red
+    Write-Host "⚠ - Creation of the folder $folderLocation..." -ForegroundColor yellow
+    New-Item $folderLocation -ItemType Directory
+}
+
+if (Get-Module -ListAvailable -Name "Posh-SSH") {
+    Import-Module Posh-SSH
+    Write-Host "✔️ - The module Posh-SSH has been detected succesfully." -ForegroundColor green
+} else {
+    Write-Host "❌ - The module Posh-SSH is not installed !" -ForegroundColor red
+    Write-Host "⚠️ - Please wait until the installation of Posh-SSH is being processed..." -ForegroundColor yellow
+    Install-Module Posh-SSH -Confirm:$false -Force
+}
+
+############################################################################################
+#                                                                                          #
+#                                4 - Backup network devices                                #
+#                                                                                          #
+############################################################################################
+
 $networkEquipments = Get-NetworkEquipments $networkEquipmentJSON
 Backup-NetworkEquipmentsConfig
 
-###############################################################################################
-#                                                                                             #
-# 5 - ARRET DU SERVICE TFTP                                                                   #
-#                                                                                             #
-###############################################################################################
+############################################################################################
+#                                                                                          #
+#                                 4 - Stopping TFTP Server                                 #
+#                                                                                          #
+############################################################################################
 
 $serviceState = Get-Service -Name "TFTPServer"
 
 if ($serviceState.Status -eq "Running") {
     Stop-Service -Name "TFTPServer"
-    Write-Host "OK - The Open TFTP Server has been stopped !" -ForegroundColor yellow
+    Write-Host "⚠️ - The Open TFTP Server has been stopped !" -ForegroundColor yellow
 } else {
-    Write-Host "OK - The service is already stopped." -ForegroundColor green
+    Write-Host "✔️ - The service is already stopped." -ForegroundColor green
 }
